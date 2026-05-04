991c75210e050d5d11d851388b5da9380ae5bd10
zadigital28-ai <zadigital28@gmail.com>
2026-05-04 15:16:07 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-/**
- * ============================================================
- * CONFIG — Ganti URL ini saat deploy untuk klien baru
- * ============================================================
- */
-window.GAS_URL = 'https://script.google.com/macros/s/AKfycbz5nS8j2tuM0KTh0NXUaSB3G07_L5LMpH5omsLGJU-6j7U3YMuVCUrpQUDWOSNJdcAksA/exec';
